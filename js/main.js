573af310dd329ac9ab00878069820f2c9af036a1
Hashim Zuraiqi <158300334+HashimZuraiqi@users.noreply.github.com>
2025-05-01 16:15:21 +0300

    added examples section and updated login page

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    console.log("jQuery is working!");
+    const templates = $('.roller-item');
+    let main_index = 0;
+
+    function updateRoller() {
+        templates.removeClass('left active right');
+        let left_index = (main_index - 1 + templates.length) % templates.length;
+        let right_index = (main_index + 1) % templates.length;
+
+        templates.eq(left_index).addClass('left');
+        templates.eq(main_index).addClass('active');
+        templates.eq(right_index).addClass('right');
+    }
+
+    $('.left-arrow').click(function () {
+        main_index = (main_index - 1 + templates.length) % templates.length;
+        updateRoller();
+    });
+
+    $('.right-arrow').click(function () {
+        main_index = (main_index + 1) % templates.length;
+        updateRoller();
+    });
+
+    updateRoller();
+});
