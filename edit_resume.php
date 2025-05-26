702ec024a80e7e0ed14dca2f31c3807d5ee33837
Faris <Fasaad2005@gmail.com>
2025-05-26 21:29:39 +0300

    Update edit_resume.php

--- edit_resume.php
+++ edit_resume.php
@@ -136,6 +136,7 @@ $conn->close();
                     <?php if (!empty($error)): ?>
                         <div class="alert alert-danger"><?php echo htmlspecialchars($error); ?></div>
                     <?php endif; ?>
+                    
                     <?php if (!empty($success)): ?>
                         <div class="alert alert-success"><?php echo htmlspecialchars($success); ?></div>
                     <?php endif; ?>
